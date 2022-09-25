08b01500fd6714ab89c912598e2936d80ccc29fb
NduvhoEdward <Nduvho.Ramashia@gmail.com>
2022-09-25 20:10:45 +0200

    Message time and frequency representation done

--- frequency_mod_scheme.m
+++ frequency_mod_scheme.m
@@ -9,29 +9,38 @@
 clear; close all;
 
 %% Initializaions
-    Fs = 1*10^6;                  % Sampling frequency
-    T = 1/Fs;                     % Sampling period
+    fs = 1*10^6;                  % Sampling frequency
+    T = 1/fs;                     % Sampling period
     L = 100000;                   % Length of signal
     t = (0:L-1)*T;                % Time vector
 
     fc = 1000; 
     Ac = 1;
     Bf = 5;
-
-    m_t1 = 2*sin(pi*100*t)./(pi*100*t) + 10.*t;
-    m_t2 = 2*sin(pi*100*t)./(pi*100*t) + (1 - 10.*t);
+    
+    m_t1 = 2*sinc(100*t) + 10.*t;
+    m_t2 = 2*sinc(100*t) + (1 - 10.*t);
 
     m_t = m_t1.*(heaviside(t)-heaviside(t-0.05)) + ...
           m_t2.*(heaviside(t-0.05)-heaviside(t-0.1)); 
     
-    c_t = Ac*cos(2*pi*fc*t);
-
+%% Message time and frequency representations
     plot(t, m_t); 
-    grid
+    grid;
+    xlim([0 0.1])
+%%
+    M_f = fft(m_t);
+    fshift = (-L/2: L/2 -1)*(fs/L);
+    ushift = fftshift(M_f);
+    plot(fshift,abs(ushift));
+    title('Double-Sided Amplitude Spectrum of m(t)');
+    xlabel('Frequency (Hz)');
+    ylabel('Magnitude');
+    xlim([-200 200])
 
-%% 
+%%
+
+%%
     
-
-
-
-
+%%    
+   

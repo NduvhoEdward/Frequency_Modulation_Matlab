60fbc72f278d8776ed9e72113203a5b5d3c444bd
NduvhoEdward <Nduvho.Ramashia@gmail.com>
2022-09-28 10:59:12 +0200

    Modulated signal time and frequency reps done. Initial steps towards demodulation done: Frequncy modulation converted to Frequency & Amplitude modulation, via a diff

--- frequency_mod_scheme.m
+++ frequency_mod_scheme.m
@@ -12,7 +12,7 @@ clear; close all;
     fs = 1*10^6;                  % Sampling frequency
     T = 1/fs;                     % Sampling period
     L = 100000;                   % Length of signal
-    t = (0:L-1)*T;                % Time vector
+    t = 0:T:0.1; %(0:L-1)*T;                % Time vector
 
     fc = 1000; 
     Ac = 1;
@@ -30,17 +30,66 @@ clear; close all;
     xlim([0 0.1])
 %%
     M_f = fft(m_t);
-    fshift = (-L/2: L/2 -1)*(fs/L);
+    fshift = (-L/2: L/2 )*(fs/L);
     ushift = fftshift(M_f);
     plot(fshift,abs(ushift));
     title('Double-Sided Amplitude Spectrum of m(t)');
     xlabel('Frequency (Hz)');
     ylabel('Magnitude');
-    xlim([-200 200])
+    xlim([-2000 2000])
+
+%%
+
+%% Modulated Signal
+    W = obw(m_t,fs); %1500;
+    m_max = max(m_t);
+    kf = Bf*W/m_max;
+    intg_m = cumtrapz(t, m_t);
+    u_t = Ac*cos(2*pi*fc*t + 2*pi*kf*intg_m);
+
+%%
+    plot(t, u_t);
+    grid;
+    xlim([0 0.1])
+%%
+    U_f = fft(u_t);
+    fshift = (-L/2: L/2)*(fs/L);
+    ushift = fftshift(U_f);
+    plot(fshift,abs(ushift));
+    title('Double-Sided Amplitude Spectrum of u(t)');
+    xlabel('Frequency (Hz)');
+    ylabel('Magnitude');
+    xlim([-1500 1500])
+
+
+%%
+    fi_t = fc + kf*m_t;
+    plot(t,fi_t);
+    grid on;
+
+%%
+    bwf = obw(fi_t,fs);
+%%  
 
 %%
 
 %%
-    
-%%    
-   
+    %2*pi*fc*t + 2*pi*kf*intg_m
+    cos_angle = acos(u_t);
+    theta = cos_angle - 2*pi*fc*t;
+%%
+
+%%
+    m_out = diff(theta);
+    m_out = m_out/max(m_out);
+    plot(t(:,1:length(m_out)),m_out);
+    grid on; 
+    %xlim([0 0.03])
+%%
+    Q = unwrap(m_out);
+    plot(t(:,1:length(Q)),Q)
+
+%%
+
+%%
+

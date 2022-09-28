097e52ccf39a8040012c2df00f5bcb123284c59c
NduvhoEdward <Nduvho.Ramashia@gmail.com>
2022-09-28 20:40:50 +0200

    Added the noise compenent to the received signal. Also added title and legends to the plots

--- frequency_mod_scheme.m
+++ frequency_mod_scheme.m
@@ -12,7 +12,7 @@ clear; close all;
     fs = 1*10^6;                  % Sampling frequency
     T = 1/fs;                     % Sampling period
     L = 100000;                   % Length of signal
-    t = 0:T:0.1; %(0:L-1)*T;                % Time vector
+    t = (0:L)*T;                % Time vector
 
     fc = 1000; 
     Ac = 1;
@@ -26,8 +26,11 @@ clear; close all;
     
 %% Message time and frequency representations
     plot(t, m_t); 
+    title('Massage Signal');
+    xlabel('Time');
+    ylabel('Amplitude');
+    xlim([0 0.1]);
     grid;
-    xlim([0 0.1])
 %%
     M_f = fft(m_t);
     fshift = (-L/2: L/2 )*(fs/L);
@@ -36,7 +39,7 @@ clear; close all;
     title('Double-Sided Amplitude Spectrum of m(t)');
     xlabel('Frequency (Hz)');
     ylabel('Magnitude');
-    xlim([-2000 2000])
+    xlim([-200 200])
 
 %%
 
@@ -47,16 +50,18 @@ clear; close all;
     intg_m = cumtrapz(t, m_t);
     u_t = Ac*cos(2*pi*fc*t + 2*pi*kf*intg_m);
 
-%%
     plot(t, u_t);
     grid;
+    title('Frequency Modulated Signal');
+    xlabel('Time');
+    ylabel('Amplitude');
     xlim([0 0.1])
 %%
     U_f = fft(u_t);
     fshift = (-L/2: L/2)*(fs/L);
     ushift = fftshift(U_f);
     plot(fshift,abs(ushift));
-    title('Double-Sided Amplitude Spectrum of u(t)');
+    title('Amplitude Spectrum of u(t)');
     xlabel('Frequency (Hz)');
     ylabel('Magnitude');
     xlim([-1500 1500])
@@ -66,53 +71,56 @@ clear; close all;
     fi_t = fc + kf*m_t;
     plot(t,fi_t);
     grid on;
+    title('Instanteneous Frequency');
+    xlabel('Time');
+    ylabel('Amplitude');
+    grid on;
 
 %%
     bwf = obw(fi_t,fs);
 %%  
 
 %%
-
-%%
-    %2*pi*fc*t + 2*pi*kf*intg_m
     cos_angle = acos(u_t);
     theta = cos_angle - 2*pi*fc*t;
-%%
-
-%%
-    m_out = diff(theta);
-    m_out = m_out/max(m_out);
-    plot(t(:,1:length(m_out)),m_out);
-    grid on; 
-    %xlim([0 0.03])
-%%
-    Q = unwrap(m_out);
-    plot(t(:,1:length(Q)),Q)
-
-%%
-    cos_angle = acos(u_t);
-    theta = cos_angle - 2*pi*fc*t;
-    m_out = diff(theta);
-    m_out = m_out/T;
+    m_out = diff(theta)/T;
     m_out(end+1)=m_out(end);
-    plot(t,m_out);
-    grid on; 
+    plot(t,m_out, 'DisplayName','Freq. and Amp. Modulated sig.');
     hold on;
-%%
+
     m_out = envelope(m_out,10,'rms');
-    plot(t,m_out)
-    grid on;
-    hold off;
-    %xlim([0 0.01])
+    plot(t,m_out, 'DisplayName','AM Envelope')
 
+    title('Demodulated Signal');
+    xlabel('Time');
+    ylabel('Amplitude');  
+
+    legend;
+    grid on;    
 %%
-
     n_t = sqrt(0.05)*randn(size(t));
     y_t = u_t + n_t;
     plot(t, y_t);
     grid on;
-
+    title('Noisy Received Signal');
+    xlabel('Time');
+    ylabel('Amplitude');  
 %%
+    cos_angle2 = acos(y_t);
+    theta2 = real(cos_angle2) - 2*pi*fc*t;
+    m_out2 = diff(theta2);
+    m_out2 = m_out2/T;
+    m_out2(end+1)=m_out2(end);
+    plot(t,m_out2,'DisplayName','Noisy Freq. & Amp. Modulated sig.'); 
+    hold on;
 
+    m_out2 = envelope(m_out2,100,'rms');
+    plot(t,m_out2,'DisplayName','Noisy signal Envelope')
+    grid on;
+    hold off;
+    xlim([0 0.01])
 
-
+    title('Demodulated Signal');
+    xlabel('Time');
+    ylabel('Amplitude');  
+%%
